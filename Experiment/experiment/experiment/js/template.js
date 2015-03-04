adfe30d9afb77ed0fc69f5b6bce78b5a3c7e9db3
Sophie Bridgers <sophie.bridgers@gmail.com>
2015-03-03 18:18:12 -0800

    updated to new experiment version

--- Experiment/experiment/experiment/js/template.js
+++ Experiment/experiment/experiment/js/template.js
@@ -17,26 +17,32 @@ function make_slides(f) {
 
   slides.single_trial = slide({
     name: "single_trial",
+    
     start: function() {
       $(".err").hide();
+      $(".err2").hide();
       $("#manipulation").html("While Vicki is outside playing, her sister,<br> Denise, moves the violin to " + exp.condition + " container.");
     },
+    
     button : function() {
       response_red = $("#text_response_red").val();
       response_green = $("#text_response_green").val();
       response_purple = $("#text_response_purple").val();
       response_blue = $("#text_response_blue").val();
-      
-      if (response_red.length == 0 | response_green.length == 0 | 
-      response_purple.length == 0 | response_blue.length == 0) {
+
+      if ($("#text_response_red").val().length == 0 || $("#text_response_green").val().length == 0 || 
+      $("#text_response_purple").val().length == 0 || $("#text_response_blue").val().length == 0) {
         $(".err").show();
       } else {
-      if (isNaN(response_red) == true | isNaN(response_green) == true | 
-      isNaN(response_purple) == true | isNaN(response_blue) == true) {
-        $(".err").show();
+      if (!(($("#text_response_red").val()!='' && $("#text_response_red").val()>=0 && $("#text_response_red").val()<=100) &&
+      ($("#text_response_green").val()!='' && $("#text_response_green").val()>=0 && $("#text_response_green").val()<=100) &&
+      ($("#text_response_purple").val()!='' && $("#text_response_purple").val()>=0 && $("#text_response_purple").val()<=100) &&
+      ($("#text_response_blue").val()!='' && $("#text_response_blue").val()>=0 && $("#text_response_blue").val()<=100))) {
+        $(".err2").show();
+        $(".err").hide();
       } else {
         exp.data_trials.push({
-          "trial_type" : "single_trial",
+          "trial_type" : "subject_responses",
           "response_red" : response_red,
           "response_green" : response_green,
           "response_purple" : response_purple,
@@ -47,6 +53,36 @@ function make_slides(f) {
     },
   });
 
+slides.color_check = slide({
+	name: "color_check",
+	start: function () {
+		$(".err3").hide();
+	},
+	button : function() {
+	
+	response_color_red = $("#text_response_color_red").val();
+	response_color_green = $("#text_response_color_green").val();
+	response_color_purple = $("#text_response_color_purple").val();
+	response_color_blue = $("#text_response_color_blue").val();
+	debugger;
+
+	if (($("#text_response_color_red").val().length == 0 || isNaN($("#text_response_color_red").val()) == false) || 
+	($("#text_response_color_green").val().length == 0 || isNaN($("#text_response_color_green").val()) == false) || 
+      ($("#text_response_color_purple").val().length == 0 || isNaN($("#text_response_color_purple").val()) == false) || 
+      ($("#text_response_color_blue").val().length == 0 || isNaN($("#text_response_color_blue").val()) == false)) {
+        $(".err3").show();
+        } else {
+        exp.data_trials.push({
+          "trial_type" : "color_check",
+          "response_color_red" : response_color_red,
+          "response_color_green" : response_color_green,
+          "response_color_purple" : response_color_purple,
+          "response_color_blue" : response_color_blue  
+        });
+        exp.go(); //make sure this is at the *end*, after you log your data
+      }
+    },
+  });
 
   slides.subj_info =  slide({
     name : "subj_info",
@@ -56,6 +92,7 @@ function make_slides(f) {
         language : $("#language").val(),
         enjoyment : $("#enjoyment").val(),
         asses : $('input[name="assess"]:checked').val(),
+        colorblind : $('input[name="colorblind"]:checked').val(),
         age : $("#age").val(),
         gender : $("#gender").val(),
         education : $("#education").val(),
@@ -97,7 +134,8 @@ function init() {
       screenUW: exp.width
     };
   //blocks of the experiment:
-  exp.structure=["i0", "instructions", "single_trial", 'subj_info', 'thanks'];
+  exp.structure=["i0", "instructions", "single_trial", "color_check", 'subj_info', 'thanks'];
+  
   
   exp.data_trials = [];
   //make corresponding slides:
